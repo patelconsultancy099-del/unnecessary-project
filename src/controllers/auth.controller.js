2f9cd49fbc77fc4b3bbc78207dec2c3b5d4830ad
patelconsultancy099-del <patelconsultancy099@gmail.com>
2026-07-31 13:26:37 +0530

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,17 +8,16 @@ function serialize(user) {
 }
 
 function setAuthCookies(res, accessToken, refreshToken) {
-  const secure = process.env.NODE_ENV === 'production';
   res.cookie('accessToken', accessToken, {
     httpOnly: true,
-    secure,
-    sameSite: 'Strict',
+    secure: true,
+    sameSite: 'None',
     maxAge: 15 * 60 * 1000
   });
   res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
-    secure,
-    sameSite: 'Strict',
+    secure: true,
+    sameSite: 'None',
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 }
